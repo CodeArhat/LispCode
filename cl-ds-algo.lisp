e5b25c49eb21af25a2c30dfc2816f1b1807f69ab
Wang Zheng <codearhat@gmail.com>
2017-07-04 11:02:20 +0800

    添加vec3f测试

--- cl-ds-algo.lisp
+++ cl-ds-algo.lisp
@@ -19,20 +19,20 @@
       (setf (aref a i) (random bound rng)))
     a))
 
-(defun shuffle-vector (a &optional (b 0) (e (length a)))
-  (let ((d (- e b)))
-    (loop for i from b to e do
-      (rotatef (aref a i) (aref a (+ b(random d)))))
-    a))
-
-(defun gen-sequence (n)
+(defun gen-sequence-vector (n)
   (let ((a (make-array n)))
     (dotimes (i n)
       (setf (aref a i) i))
     a))
 
+(defun shuffle-vector (a &optional (b 0) (e (length a)))
+  (let ((d (- e b)))
+    (loop for i from b to (1- e) do
+      (rotatef (aref a i) (aref a (+ b (random d)))))
+    a))
+
 (defun gen-shuffle-vector (n)
-  (shuffle-vector (gen-sequence n)))
+  (shuffle-vector (gen-sequence-vector n)))
 
 ;; same as logcount
 (defun count-ones-v1 (n)
@@ -151,3 +151,39 @@
   (x 0.0 :type float)
   (y 0.0 :type float)
   (z 0.0 :type float))
+
+(defun vec3f-zero (v)
+  (setf (vec3f-x v) 0.0
+        (vec3f-y v) 0.0
+        (vec3f-z v) 0.0))
+
+(defun vec3f-unitx () (make-vec3f :x 1.0))
+(defun vec3f-unity () (make-vec3f :y 1.0))
+(defun vec3f-unitz () (make-vec3f :z 1.0))
+
+(defun vec3f-dot (va vb)
+  (+ (* (vec3f-x va) (vec3f-x vb))
+     (* (vec3f-y va) (vec3f-y vb))
+     (* (vec3f-z va) (vec3f-z vb))))
+
+(defun vec3f-cross (va vb)
+  (declare (optimize (speed 3) (safety 0) (space 0)))
+  (let ((ax (vec3f-x va))
+        (ay (vec3f-y va))
+        (az (vec3f-z va))
+        (bx (vec3f-x vb))
+        (by (vec3f-y vb))
+        (bz (vec3f-z vb)))
+    (make-vec3f :x (- (* ay bz) (* az by))
+                :y (- (* az bx) (* ax bz))
+                :z (- (* ax by) (* ay bx)))))
+
+(defun vec3f-test (n)
+  ;; CL:C++ ~= 17
+  (let ((va (vec3f-unitx))
+        (vb (vec3f-unity))
+        (vt))
+    (dotimes (i n)
+      (setf vt (vec3f-cross va vb))
+      (setf va vb vb vt))
+    va))
